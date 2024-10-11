93e17fe8c47f24539de6c016ab0a4b8f564126c5
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <63088266+sbi1024@users.noreply.github.com>
2024-10-11 23:02:12 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 68.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] result = new int[2];
+        result[0] = money / 5500;
+        result[1] = money % 5500;
+        return result;
+    }
+}
