568a1deb31148f0467ad96f503ec8ddb5bce7ad4
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <63088266+sbi1024@users.noreply.github.com>
2024-10-11 20:39:46 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.23 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,7 +1,20 @@
 import java.util.Arrays;
 class Solution {
     public int solution(int[] array) {
-        Arrays.sort(array);
-        return array[(array.length / 2 )];
+        int answer = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j < array.length; j++) {
+                if (array[j] < array[i]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+
+        answer = array[array.length / 2];
+
+        return answer;
     }
 }
