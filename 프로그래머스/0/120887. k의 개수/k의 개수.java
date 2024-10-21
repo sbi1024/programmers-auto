54a473b43306df7241678c823feea4bf34e91601
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <63088266+sbi1024@users.noreply.github.com>
2024-10-21 11:10:14 +0900

    [level 0] Title: k의 개수, Time: 114.67 ms, Memory: 153 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int i, int j, int k) {
         int count = 0;
+        String strK = String.valueOf(k);
         for (int first = i ; first <= j; first++) {
             String[] split = String.valueOf(first).split("");
             for (String str : split) {
-                if (str.equals(String.valueOf(k))) {
+                if (str.equals(strK)) {
                     count ++;
                 }   
             }
