7e79e70c6eaaf69952f997c283634b458dcfa944
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <63088266+sbi1024@users.noreply.github.com>
2024-10-31 22:45:58 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,21 +1,10 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        StringBuilder sb = new StringBuilder();
-        char numStr1 = my_string.charAt(num1);
-        char numStr2 = my_string.charAt(num2);
-        
         char[] chArr = my_string.toCharArray();
-        for (int i = 0 ; i < chArr.length; i++) {
-            if (i == num1) {
-                sb.append(numStr2);
-            } 
-            else if (i == num2) {
-                sb.append(numStr1);
-            } else {
-                sb.append(chArr[i]);
-            }
-        }
-        
-        return sb.toString();
+        char chNum1 = chArr[num2];
+        char chNum2 = chArr[num1];
+        chArr[num1] = chNum1;
+        chArr[num2] = chNum2;
+        return String.valueOf(chArr);
     }
 }
