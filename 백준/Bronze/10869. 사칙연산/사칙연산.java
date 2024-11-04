998514627d38dd31eb404347eb562cc1f9620709
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <63088266+sbi1024@users.noreply.github.com>
2024-11-04 17:28:05 +0900

    [Bronze V] Title: 사칙연산, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scann = new Scanner(System.in);
+		 
+		 int a,b;
+		 a = scann.nextInt();
+		 b = scann.nextInt();
+		
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+	}
+
+}
