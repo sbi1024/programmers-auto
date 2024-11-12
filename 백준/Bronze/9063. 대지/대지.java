e54ca800faa5d33c284e40362550aaeef57cb475
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <63088266+sbi1024@users.noreply.github.com>
2024-11-12 10:16:19 +0900

    [Bronze III] Title: 대지, Time: 276 ms, Memory: 32848 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.java
+++ 백준/Bronze/9063. 대지/대지.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,19 +10,22 @@ public class Main {
             System.out.println(0);
             return;
         }
-        int[] xArray = new int[count];
-        int[] yArray = new int[count];
+
+        int minX = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxY = Integer.MIN_VALUE;
+
         for (int i = 0; i < count; i++) {
             String[] splitStr = br.readLine().split(" ");
-            xArray[i] = Integer.parseInt(splitStr[0]);
-            yArray[i] = Integer.parseInt(splitStr[1]);
+            int x = Integer.parseInt(splitStr[0]);
+            int y = Integer.parseInt(splitStr[1]);
+
+            if (x < minX) minX = x;
+            if (x > maxX) maxX = x;
+            if (y < minY) minY = y;
+            if (y > maxY) maxY = y;
         }
-
-        int minX = Arrays.stream(xArray).min().getAsInt();
-        int minY = Arrays.stream(yArray).min().getAsInt();
-
-        int maxX = Arrays.stream(xArray).max().getAsInt();
-        int maxY = Arrays.stream(yArray).max().getAsInt();
 
         System.out.println((maxX - minX) * (maxY - minY));
     }
