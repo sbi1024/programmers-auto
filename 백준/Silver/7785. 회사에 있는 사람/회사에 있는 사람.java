e736de52ae40cc8522fc04d03ce9fe50f67fddfb
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <63088266+sbi1024@users.noreply.github.com>
2024-11-13 11:38:14 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 1080 ms, Memory: 55584 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Set<String> set = new HashSet<>();
+        int forCount = Integer.parseInt(br.readLine());
+        for (int i = 0; i < forCount; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String status = st.nextToken();
+            if (status.equals("enter")) {
+                set.add(name);
+            } else {
+                set.remove(name);
+            }
+        }
+
+        TreeSet<String> sortSet = new TreeSet<>(set);
+        Iterator<String> iterator = sortSet.descendingIterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
+
