70bdc29b4ac3e982dd035938e025421308163511
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <63088266+sbi1024@users.noreply.github.com>
2024-11-13 10:23:09 +0900

    [Silver V] Title: 숫자 카드, Time: 852 ms, Memory: 151780 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,39 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        // 첫 번째 입력: 가지고 있는 카드의 개수 (사용하지 않음)
         br.readLine();
         String[] getCartSplit = br.readLine().split(" ");
-        int findCount = Integer.parseInt(br.readLine());
-        int[] result = new int[findCount];
+        
+        // 가지고 있는 카드를 Set에 저장
+        Set<Integer> cardSet = new HashSet<>();
+        for (String card : getCartSplit) {
+            cardSet.add(Integer.parseInt(card));
+        }
+
+        // 두 번째 입력: 찾아야 할 카드의 개수 (사용하지 않음)
+        br.readLine();
         String[] findCardSplit = br.readLine().split(" ");
 
-        Map<Integer, Boolean> cardMap = new HashMap<>();
-        Map<Integer, Boolean> findMap = new LinkedHashMap<>();
-
-        for (String getCard : getCartSplit) cardMap.put(Integer.parseInt(getCard), true);
-        for (String findCard : findCardSplit) findMap.put(Integer.parseInt(findCard), true);
-
-        int index = 0;
-        for (Integer findKey : findMap.keySet()) {
-            Boolean check = cardMap.getOrDefault(findKey, false);
-            if (check) {
-                result[index] = 1;
-            }
-            index++;
-        }
-
+        // 결과를 StringBuilder에 바로 추가
         StringBuilder sb = new StringBuilder();
-        for (int value : result) {
-            sb.append(value).append(" ");
+        for (String findCard : findCardSplit) {
+            int card = Integer.parseInt(findCard);
+            sb.append(cardSet.contains(card) ? 1 : 0).append(" ");
         }
+
+        // 최종 출력
         System.out.println(sb);
     }
 }
