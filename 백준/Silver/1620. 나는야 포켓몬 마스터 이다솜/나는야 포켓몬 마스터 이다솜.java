174e7f2dc82954e37ba363a244b64b3c5dd3d642
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <63088266+sbi1024@users.noreply.github.com>
2024-11-14 10:07:08 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 1408 ms, Memory: 103956 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int inputCount = Integer.parseInt(st.nextToken());
+        int findCount = Integer.parseInt(st.nextToken());
+
+        Map<String, Integer> inputStringMap = new HashMap<>();
+        Map<Integer, String> inputIntMap = new HashMap<>();
+
+        for (int i = 1; i <= inputCount + findCount; i++) {
+            if (i <= inputCount) {
+                String input = br.readLine();
+                inputStringMap.put(input, i);
+                inputIntMap.put(i, input);
+            } else {
+                String findValue = br.readLine();
+                try {
+                    int index = Integer.parseInt(findValue);
+                    String name = inputIntMap.get(index);
+                    System.out.println(name);
+                } catch (NumberFormatException e) {
+                    Integer index = inputStringMap.get(findValue);
+                    System.out.println(index);
+                }
+            }
+        }
+    }
+}
