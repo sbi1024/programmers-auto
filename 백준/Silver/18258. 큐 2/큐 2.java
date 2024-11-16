f93e91500a748123f0589c7ef1da4a3ca6ca2882
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <63088266+sbi1024@users.noreply.github.com>
2024-11-16 09:34:26 +0900

    [Silver IV] Title: 큐 2, Time: 992 ms, Memory: 243096 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -10,31 +10,21 @@ public class Main {
         int forCount = Integer.parseInt(br.readLine());
         Deque<Integer> deque = new ArrayDeque<>();
         StringBuilder sb = new StringBuilder();
+        
         for (int i = 0; i < forCount; i++) {
-            String[] command = br.readLine().split(" ");
-            switch (command[0]) {
-                case "push":
-                    deque.addLast(Integer.parseInt(command[1]));
-                    break;
-                case "pop":
-                    if (deque.isEmpty()) sb.append(-1).append("\n");
-                    else sb.append(deque.removeFirst()).append("\n");
-                    break;
-                case "size":
-                    sb.append(deque.size()).append("\n");
-                    break;
-                case "empty":
-                    if (deque.isEmpty()) sb.append(1).append("\n");
-                    else sb.append(0).append("\n");
-                    break;
-                case "front":
-                    if (deque.isEmpty()) sb.append(-1).append("\n");
-                    else sb.append(deque.peekFirst()).append("\n");
-                    break;
-                case "back":
-                    if (deque.isEmpty()) sb.append(-1).append("\n");
-                    else sb.append(deque.peekLast()).append("\n");
-                    break;
+            String command = br.readLine();
+            if (command.startsWith("push")) {
+                deque.addLast(Integer.parseInt(command.substring(5)));
+            } else if (command.equals("pop")) {
+                sb.append(deque.isEmpty() ? -1 : deque.removeFirst()).append("\n");
+            } else if (command.equals("size")) {
+                sb.append(deque.size()).append("\n");
+            } else if (command.equals("empty")) {
+                sb.append(deque.isEmpty() ? 1 : 0).append("\n");
+            } else if (command.equals("front")) {
+                sb.append(deque.isEmpty() ? -1 : deque.peekFirst()).append("\n");
+            } else if (command.equals("back")) {
+                sb.append(deque.isEmpty() ? -1 : deque.peekLast()).append("\n");
             }
         }
         System.out.println(sb);
