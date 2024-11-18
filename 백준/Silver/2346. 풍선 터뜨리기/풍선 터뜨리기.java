883781f556d32b7b5475e13e9c8de269498d91dd
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <63088266+sbi1024@users.noreply.github.com>
2024-11-18 22:39:11 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 152 ms, Memory: 16084 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -1,35 +1,39 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Deque<Map<Integer, Integer>> deque = new ArrayDeque<>();
+        Deque<int[]> deque = new ArrayDeque<>();
+
         int forCount = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         for (int i = 1; i <= forCount; i++) {
-            Map<Integer, Integer> map = new HashMap<>();
             int value = Integer.parseInt(st.nextToken());
-            map.put(i, value);
-            deque.addLast(map);
+            deque.addLast(new int[]{i, value});
         }
-        Map<Integer, Integer> remove = deque.removeFirst();
-        Integer value = remove.get(1);
-        sb.append(1).append(" ");
+
+        int[] removeArray = deque.removeFirst();
+        int value = removeArray[1];
+        sb.append(removeArray[0]).append(" ");
+
         while (!deque.isEmpty()) {
             if (value > 0) {
                 for (int i = 0; i < value - 1; i++) deque.addLast(deque.removeFirst());
-                remove = deque.removeFirst();
+                removeArray = deque.removeFirst();
             } else {
-                for (int i = 0; i < Math.abs(value) - 1; i++) deque.addFirst(deque.removeLast());
-                remove = deque.removeLast();
+                for (int i = 0; i < (-value) - 1; i++) deque.addFirst(deque.removeLast());
+                removeArray = deque.removeLast();
             }
-            Integer index = new ArrayList<>(remove.keySet()).get(0);
-            sb.append(index).append(" ");
-            value = remove.get(index);
+            value = removeArray[1];
+            sb.append(removeArray[0]).append(" ");
         }
+
         bw.write(sb.toString());
         bw.flush();
         bw.close();
