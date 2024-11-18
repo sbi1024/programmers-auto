1d37b6f0060812089efe2834fc05c80707ce5156
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <63088266+sbi1024@users.noreply.github.com>
2024-11-18 14:26:53 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 112 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Deque<Integer> deque1 = new ArrayDeque<>();
+        Deque<Integer> deque2 = new ArrayDeque<>();
+
+        br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) deque1.add(Integer.parseInt(st.nextToken()));
+
+
+        int stand = 1;
+        while (!deque1.isEmpty()) {
+            Integer value = deque1.removeFirst();
+            if (value == stand) stand++;
+            else deque2.addFirst(value);
+            while (!deque2.isEmpty() && (deque2.peekFirst() == stand)) {
+                deque2.removeFirst();
+                stand++;
+            }
+        }
+
+        if (deque2.isEmpty()) bw.write("Nice");
+        else bw.write("Sad");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
