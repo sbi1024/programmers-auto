0da8d28ae1de9557f281c99c2786ef213973c4eb
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <63088266+sbi1024@users.noreply.github.com>
2024-11-21 22:27:10 +0900

    [Silver II] Title: 베르트랑 공준, Time: 164 ms, Memory: 17004 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,33 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        boolean[] isPrime = new boolean[123456 * 2 + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+        for (int i = 2; i * i <= isPrime.length; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < isPrime.length; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
         int input;
         while ((input = Integer.parseInt(br.readLine())) != 0) {
             int count = 0;
             for (int i = input + 1; i <= input * 2; i++) {
-                if (isPrime(i)) {
+                if (isPrime[i]) {
                     count++;
                 }
             }
             sb.append(count).append("\n");
         }
-        System.out.println(sb);
-    }
 
-    public static boolean isPrime(int input) {
-        if (input <= 1) return false;
-        if (input <= 3) return true;
-        if (input % 2 == 0 || input % 3 == 0) return false;
-        for (int i = 2; i <= Math.sqrt(input); i++) {
-            if (input % i == 0) {
-                return false;
-            }
-        }
-        return true;
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
