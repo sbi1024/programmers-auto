bcf15a7e374764ea4bf4d5b1a8801b9550b5b2a5
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <63088266+sbi1024@users.noreply.github.com>
2024-11-29 15:52:43 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 108 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int input = Integer.parseInt(br.readLine());
+
+        int[] array = new int[21];
+        array[0] = 0;
+        array[1] = 1;
+        for (int i = 2; i < array.length; i++) {
+            array[i] = array[i - 1] + array[i - 2];
+        }
+
+        sb.append(array[input]);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
