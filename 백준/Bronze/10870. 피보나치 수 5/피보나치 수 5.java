520f767b23ad9e4aeb888716edd811e9ac3cb1d7
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <63088266+sbi1024@users.noreply.github.com>
2024-11-30 14:21:22 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -1,26 +1,25 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
 
-        int input = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int result = recursion(n);
+        System.out.println(result);
 
-        int[] array = new int[21];
-        array[0] = 0;
-        array[1] = 1;
-        for (int i = 2; i < array.length; i++) {
-            array[i] = array[i - 1] + array[i - 2];
-        }
-
-        sb.append(array[input]);
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
         br.close();
     }
 
+    public static int recursion(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n <= 2) {
+            return 1;
+        }
+        return recursion(n - 1) + recursion(n - 2);
+    }
 }
